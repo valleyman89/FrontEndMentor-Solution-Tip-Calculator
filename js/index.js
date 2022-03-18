53586314297a48cf0c7887cfe438844736bdbae0
valleyman89 <dev@stevenrolph.com>
2022-03-18 16:30:19 +0000

    tidy up error function

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
+function clearError() {
+  document.getElementById("error").innerText = "";
+  let input = document.getElementById("inputPeople");
+  input.classList.remove("input--error");
+}
+
 function clearSplitter() {
   document.getElementById("tipTotal").innerText = "0";
   document.getElementById("resultTotal").innerText = "0";
+  clearError();
 }
 
 window.onload = clearSplitter;
@@ -9,26 +16,31 @@ function calculateTip() {
   let bill = parseFloat(document.getElementById("inputBill").value);
   let people = parseInt(document.getElementById("inputPeople").value);
 
-  if (document.getElementById("five").checked) {
-    percent = 0.05;
-  } else if (document.getElementById("ten").checked) {
-    percent = 0.1;
-  } else if (document.getElementById("fifteen").checked) {
-    percent = 0.15;
-  } else if (document.getElementById("twenty-five").checked) {
-    percent = 0.25;
-  } else if (document.getElementById("fifty").checked) {
-    percent = 0.5;
+  if (people === 0) {
+    document.getElementById("error").innerText = "Can't be zero";
+    let input = document.getElementById("inputPeople");
+    input.classList.add("input--error");
   } else {
-    percent = 0;
+    clearError();
+    if (document.getElementById("five").checked) {
+      percent = 0.05;
+    } else if (document.getElementById("ten").checked) {
+      percent = 0.1;
+    } else if (document.getElementById("fifteen").checked) {
+      percent = 0.15;
+    } else if (document.getElementById("twenty-five").checked) {
+      percent = 0.25;
+    } else if (document.getElementById("fifty").checked) {
+      percent = 0.5;
+    } else {
+      percent = 0;
+    }
+
+    let tipPerPerson = ((bill * percent) / people).toFixed(2);
+    let totalPerson = bill / people;
+    let total = parseFloat(totalPerson) + parseFloat(tipPerPerson);
+
+    document.getElementById("tipTotal").innerText = parseFloat(tipPerPerson);
+    document.getElementById("resultTotal").innerText = total.toFixed(2);
   }
-
-  let tipPerPerson = ((bill * percent) / people).toFixed(2);
-  let totalPerson = bill / people;
-  let total = parseFloat(totalPerson) + parseFloat(tipPerPerson);
-
-  document.getElementById("tipTotal").innerText = parseFloat(tipPerPerson);
-  document.getElementById("resultTotal").innerText = total.toFixed(2);
 }
-
-calculateTip();
